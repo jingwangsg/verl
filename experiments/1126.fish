07e5deb30ff27d1c0580b54d6a4cbbf4c40c2f8c
jingwang <jingwang@nvidia.com>
2025-11-26 19:36:14 +0800

    Add FrameThinker training jobs

--- experiments/1126.fish
+++ experiments/1126.fish
@@ -1,3 +1,11 @@
+set DATA_DIR /mnt/amlfs-02/shared/datasets/s3/video_reason/
+set VH_TRAIN $DATA_DIR/Video-Holmes/train.parquet
+set VH_TEST $DATA_DIR/Video-Holmes/test.parquet
+set LVR_3K $DATA_DIR/LongVideoReason/train_mcq_3k.parquet
+set LVR_10K $DATA_DIR/LongVideoReason/train_mcq_10k.parquet
+set LVR_TEST $DATA_DIR/LongVideoReason/test.parquet
+
+
 EXP=framethinker_coldstart \
 ray_job_submit --no-wait --skip-exists --runtime-env ../runtime_env.yaml \
     --submission-id $EXP -- bash -c \
@@ -18,3 +26,19 @@ ray_job_submit --no-wait --skip-exists --runtime-env ../runtime_env.yaml \
     algorithm.rollout_correction.rollout_is_threshold=2.0 \
     algorithm.rollout_correction.rollout_is_batch_normalize=true"""
 
+
+EXP=framethinker_coldstart_vh+lvr3k \
+ray_job_submit --no-wait --skip-exists --runtime-env ../runtime_env.yaml \
+    --submission-id $EXP -- bash -c \
+    """EXP=$EXP bash examples/framethinker/train_frame_thinker.sh \
+    data.train_files=[$VH_TRAIN,$LVR_3K] \
+    data.val_files=[$VH_TEST,$LVR_TEST] \
+    """
+
+EXP=framethinker_coldstart_vh+lvr10k \
+ray_job_submit --no-wait --skip-exists --runtime-env ../runtime_env.yaml \
+    --submission-id $EXP -- bash -c \
+    """EXP=$EXP bash examples/framethinker/train_frame_thinker.sh \
+    data.train_files=[$VH_TRAIN,$LVR_10K] \
+    data.val_files=[$VH_TEST,$LVR_TEST] \
+    """
